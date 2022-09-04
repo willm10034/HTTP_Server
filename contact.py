46f13e1b850d9e1b075d5c49e03a779df7ba387f
maskedbandit6 <bandit.masked@gmail.com>
2022-09-03 17:06:35 -0700

    Add files via upload

--- contact.py
+++ contact.py
@@ -19,5 +19,5 @@ message = parsed_message[1]
 if __name__ == '__main__':
     print('<html><body bgcolor="#808080"><center><table cellspacing="0"><tr height="32">')
     print('<td bgcolor="#202020"><center><font face="courier" size="-1" color="#ffffff">Contact</font></center></td></tr>')
-    print('<tr><td bgcolor="#ffffff"><center><font face="courier" size="-1">Information received<br>Subject: ' + subject + '<br>Message: ' + message + '</font></center></td></tr>')
+    print('<tr><td bgcolor="#ffffff" width="500" height="300"><center><font face="courier" size="-1">Information received<br>Subject: ' + subject + '<br>Message: ' + message + '</font></center></td></tr>')
     print('<tr height="32"><td bgcolor="#202020"><font face="courier" size="-1">&nbsp;</font></td></tr></table></center></body></html>')
