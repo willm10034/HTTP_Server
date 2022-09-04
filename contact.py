bb733ae9ce4d183956e2eea6ca1525b5f1df03f1
maskedbandit6 <bandit.masked@gmail.com>
2022-09-04 14:06:31 -0700

    Add files via upload

--- contact.py
+++ contact.py
@@ -1,5 +1,7 @@
 import sys
 from urllib import parse
+import smtplib
+from email.message import EmailMessage
 
 
 def get_template():
@@ -12,6 +14,19 @@ def span(in_text):
     return '<span style="font-family:courier;font-size:14px">' + in_text + '</span>'
 
 
+def send_mail(to_email, subject, message, server, from_email, user, password):
+    msg = EmailMessage()
+    msg['Subject'] = subject
+    msg['From'] = from_email
+    msg['To'] = ', '.join(to_email)
+    msg.set_content(message)
+    server = smtplib.SMTP(server)
+    server.set_debuglevel(1)
+    server.login(user, password)  # user & password
+    server.send_message(msg)
+    server.quit()
+
+
 data = sys.argv
 
 full_message = data[1]
@@ -20,6 +35,7 @@ full_message = parse.unquote_plus(full_message)
 params = {x[0]: x[1] for x in [x.split("=") for x in full_message.split("&")]}
 
 if __name__ == '__main__':
+    # send_mail(to_email=['your_email@gmail.com', 'some_other@gmail.com'], subject=params['subject'], message=params['message'], server='smtp.gmail.com', from_email=params['email'], user='user@some.com', password='password')
     response = get_template()
     response = response.replace('#header#', 'Contact')
     response = response.replace('#body#', span('<b>Information received</b><br>Name: ' + params['name'] + '<br>Email: ' + params['email'] + '<br>Subject: ' + params['subject'] + '<br>Message: ' + params['message']))
